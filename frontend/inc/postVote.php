590b6d80a8db88757d855d4bbee4aa005a6fec48
gitteri <ilan.gitter@gmail.com>
2014-10-12 17:20:34 -0400

    Working AJAX userVote

--- frontend/inc/postVote.php
+++ frontend/inc/postVote.php
@@ -1,8 +1,11 @@
 <?php   
+include "db_conx.php";
 if (isset($_POST['voteUser'])) {
-    echo "HOLA!";
     $date = date("Y-m-d");
     $userVote = $_POST['voteUser'];
+    $user_id = $_POST['user_id'];
+    $billId = $_POST['billId'];
+    $org_id = $_POST['org_id'];
     $stmt = $pdo->prepare("CALL insertUserVote(?, ?, ?, ?, ?)");
     $stmt->bindParam(1, $billId, PDO::PARAM_STR);
     $stmt->bindParam(2, $user_id, PDO::PARAM_STR);
@@ -16,10 +19,18 @@ if (isset($_POST['voteUser'])) {
     $rs = $stmt->execute();
     if ($result = $stmt->fetchAll(PDO::FETCH_ASSOC)) {
         $vote = $result[0]['vote'];
-
+        if ($vote == 'pass') {
+            echo '<input type="radio" class="votePass" name="voteUser" id="pass'.$billId.'" value="pass" checked />';
+        }
+        echo '<label for="pass'.$billId.'">PASS<span></span></label>';
+        if ($vote == 'reject') {
+            echo '<input type="radio" class="voteReject" name="voteUser" id="reject'.$billId.'" value="reject" checked />';
+        }
+        echo '<label for="reject'.$billId.'">REJECT<span></span></label>';
+    return $billId;
     }
 } else {
-    echo "HELLO";
+    echo "<p>You are not logged in. Please login and try again.</p>";
     // logged out
 }
     ?>
